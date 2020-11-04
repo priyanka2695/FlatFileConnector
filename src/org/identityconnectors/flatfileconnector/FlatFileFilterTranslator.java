89b7046f2ba9e281985a9c910c47a0e2447c4f25
priyanka.r <priyanka.r@8KCHNLAP074.8kmiles.local>
2020-11-04 11:14:27 +0530

    Updated Source

---
+++ src/org/identityconnectors/flatfileconnector/FlatFileFilterTranslator.java
@@ -0,0 +1,26 @@
+
+package org.identityconnectors.flatfileconnector;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.identityconnectors.framework.common.objects.Attribute;
+import org.identityconnectors.framework.common.objects.filter.AbstractFilterTranslator;
+import org.identityconnectors.framework.common.objects.filter.StartsWithFilter;
+
+public class FlatFileFilterTranslator extends AbstractFilterTranslator<Map<String, String>> {
+
+	@Override
+	protected Map<String, String> createStartsWithExpression(StartsWithFilter startFilter, boolean not) {
+
+		Map<String, String> startsMap = new HashMap<String, String>();
+		System.out.println("#### Inside FlatFileFilterTranslator #### ");
+		String queryString = new String();
+		Attribute attr = startFilter.getAttribute();
+		System.out.println(" ## Attribute Name " + attr.getName() + " Value" + attr.getValue().get(0).toString());
+		startsMap.put(attr.getName(), attr.getValue().get(0).toString());
+		return startsMap;
+
+	}
+
+}
